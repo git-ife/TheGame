134a39df2aca2f1a35ab33c74133928d6e632a57
git-ife <118533964+git-ife@users.noreply.github.com>
2022-11-20 10:25:19 +0100

    Test

---
+++ TheGame_Java/src/Test.java
@@ -0,0 +1,13 @@
+
+import java.applet.*;
+import java.awt.*;
+
+/* <applet code="myapplet" height="100" width="200"></applet> */
+
+public class Test extends Applet {
+
+    public void paint(Graphics g) {
+        g.drawString("welcome to applet", 150, 150);
+    }
+
+}
